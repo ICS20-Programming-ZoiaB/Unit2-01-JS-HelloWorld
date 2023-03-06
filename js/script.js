b61ab80692e3a5784cfbf9ef2b3aa1fc7e48b473
ZoiaBaran <124380748+ZoiaBaran@users.noreply.github.com>
2023-03-06 16:33:24 +0000

    added more style and favicon

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Copyright (c) 2022 ZoiaB All rights reserved
+//
+// Created by: ZoiaB
+// Created on: Mar 2022
+// This file contains the JS functions for index.html
+
+function myButtonClicked () {
+  // this function displays "Hello, World!"
+  
+  document.getElementById("hello-world").innerHTML = "This is my JavaScript. My webpage contains JavaScript that reacts to the button being clicked."
+}
